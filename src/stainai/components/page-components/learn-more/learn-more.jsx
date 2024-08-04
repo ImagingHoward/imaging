27b64920ac5d9b1f2ce394775aefee4b22967034
PathoRadi <132098084+PathoRadi@users.noreply.github.com>
2024-08-03 22:59:41 -0400

    learn more

--- src/stainai/components/page-components/learn-more/learn-more.jsx
+++ src/stainai/components/page-components/learn-more/learn-more.jsx
@@ -7,16 +7,25 @@ import axios from "axios";
 import framwork from "../../../assets/framework.png";
 import bottom from "../../../assets/bottom.png";
 import morphotype_Icon from "../../../assets/morphotype_Icon.png";
+import morphotype_Icon_section from "../../../assets/morphotype_Icon_section.png";
+import morphotype_Icon_fig from "../../../assets/morphotype_Icon_fig.png";
+
 import morphometry_Icon from "../../../assets/morphometry_Icon.png";
+import morphometry_fig from "../../../assets/morphometry_fig.png";
+import morphometry_rainbow from "../../../assets/morphometry_rainbow.png";
+
 import whole_brain_Icon  from "../../../assets/whole_brain_Icon.png";
+import whole_brain_fig from "../../../assets/whole_brain_fig.png";
 
 import { RiArrowDownSFill } from "react-icons/ri";
 import { RiArrowDropUpFill } from "react-icons/ri";
 
 const LearnMore = () => {
-  const [morphotype, SetMorphotype] = useState(false);
+  const [morphotypeExpand, setMorphotypeExpand] = useState(false);
+  const [morphometryExpand, setMorphometryExpand] = useState(false);
+  const [wholeBrainExpand, setWholeBrainExpand] = useState(false);
   
-  
+
   return (
     <div className={classes.wrapper}>
       <div className={classes.header}>
@@ -57,45 +66,99 @@ const LearnMore = () => {
 
           <div className={classes.profolioSection}>
             <div className={classes.profolioSectionHeader}>
-              <img src={morphotype_Icon} />
-              <div className={classes.title}>Morphotype mapping</div>
+              <div className={classes.title}> 
+                  <img src={morphotype_Icon} />
+                  Morphotype mapping
+                </div>
+              <div className={classes.show} onClick={()=>  setMorphometryExpand(!morphometryExpand)}>
+                Show more
+                {morphometryExpand ? <RiArrowDropUpFill size={45} /> : <RiArrowDownSFill size={35} />}
+              </div>
             </div>
-            <div className={classes.show}>
-              Show more
-              {!morphotype ? <RiArrowDownSFill size={30} /> : <RiArrowDropUpFill size={30} />}
+      
+            {
+              morphometryExpand &&
+              <div style={{ position: "relative" }}>
+                <div style={{ position: "absolute", top: "-6vh", left: "11vw"}}> <i>Allow ROI analysis for region-specific comparison.</i> </div>
+                <div style={{ display: 'flex',  alignItems: 'center', padding: '0 10%', position: "relative" }}>
+                  <div >
+                    <p>
+                      In cardiac arrest, microglia activation is prominent associated with regional vulnerability.
+                    </p>
+                    <p>
+                      Label every cell with morphotype ID. Convert Iba1 images to color-coded quantitative maps. Simplify cell quantifications by drawing ROIs.
+                    </p>
+                  </div>
+                  <div>
+                    <img src={morphotype_Icon_section} style={{width: '450px'}}/>
+                  </div>
+                </div>
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                  <img src={morphotype_Icon_fig} style={{width: '900px'}}/>
+                </div>
+              </div>
+            }
+          </div>
+
+
+          <div className={classes.profolioSection}>
+            <div className={classes.profolioSectionHeader}>
+              <div className={classes.title}> <img src={morphometry_Icon} /> Morphometry mapping</div>
+              <div className={classes.show} onClick={()=>  setMorphotypeExpand(!morphotypeExpand)}>
+                Show more 
+                {morphotypeExpand ? <RiArrowDropUpFill size={45} /> : <RiArrowDownSFill size={35} />}
+              </div>
             </div>
+         
+            {
+              morphotypeExpand &&
+              <div style={{ position: "relative" }}>
+                <div style={{ position: "absolute", top: "-6vh", left: "11vw"}}> 
+                  <i>More insights into hetergeneous microglial morphology.</i> 
+                </div>
+                <div style={{ display: 'flex',  alignItems: 'center', padding: '0 10%', position: "relative" }}>
+                  <div >
+                    <p>
+                      StainAI derives more than 28 morphometric metrics for microglia providing parameters such as cell and soma area, perimeter, shape, skletonization, intracellular distances etc.
+                    </p>
+                    <img src={morphometry_fig} style={{width: '600px'}}/>
+
+                  </div>
+                  <div>
+                    <img src={morphometry_rainbow} style={{width: '700px'}}/>
+                  </div>
+                </div>
+              </div>
+            }
           </div>
 
           <div className={classes.profolioSection}>
             <div className={classes.profolioSectionHeader}>
-              <img src={morphometry_Icon} />
-              <div className={classes.title}>Morphometry mapping</div>
+              <div className={classes.title}> <img src={whole_brain_Icon} /> Whole brain distributiong</div>
+              <div className={classes.show} onClick={()=>  setWholeBrainExpand(!wholeBrainExpand)}>
+                Show more
+                {wholeBrainExpand ? <RiArrowDropUpFill size={45} /> : <RiArrowDownSFill size={35} />}
+              </div>
             </div>
-            <div className={classes.show}>
-              Show more 
-              {<RiArrowDropUpFill size={40} />}
-            </div>
-          </div>
-
-          <div className={classes.profolioSection}>
-            <div className={classes.profolioSectionHeader}>
-              <img src={morphometry_Icon} />
-              <div className={classes.title}>Morphometry mapping</div>
-            </div>
-            <div className={classes.show}>
-              Show more 
-              {<RiArrowDownSFill size={30} />}
-            </div>
-          </div>
-
-          <div className={classes.profolioSection}>
-            <div className={classes.profolioSectionHeader}>
-              <img src={whole_brain_Icon} />
-              <div className={classes.title}>Whole brain distributiong</div>
-            </div>
-            <div className={classes.show}>
-              Show more <RiArrowDownSFill size={30} />
-            </div>
+         
+            {
+              wholeBrainExpand &&
+              <div style={{ position: "relative" }}>
+                <div style={{ position: "absolute", top: "-6vh", left: "11vw"}}> 
+                  <i>Reveals microglial activation pattern throughout the entire brain.</i> 
+                </div>
+                <div style={{ display: 'flex', padding: '3% 10%', position: "relative" }}>
+                  <div >
+                    <p>
+                      3D distribution of microglial morphotype and morphometrics.
+                    </p>  
+                  </div>
+                  <div>
+                    <img src={whole_brain_fig} style={{width: '650px'}}/>
+                  </div>
+                </div>
+              </div>
+            }
           </div>
         </div>
         <div className={classes.bottom}>
