83fa63e26e058228b55f90aa9f594ca13b2e5ef8
Darren Liu <imaging.howard@gmail.com>
2025-05-06 15:46:45 -0400

    Update essiet.component.jsx

--- src/mil/componets/page-components/people/person/essiet.component.jsx
+++ src/mil/componets/page-components/people/person/essiet.component.jsx
@@ -7,7 +7,7 @@ import essiet from "../../../../assets/images/essiet-adidongEtte.jpeg";
 const Essiet = () => {
   const person = {
     image: essiet,
-    name: "Essiet-Adidiong Ette1",
+    name: "Essiet-Adidiong Ette",
     title: "CZI NDCF Research Fellow",
     phone: "",
     email: "essietadid.ette@howard.edu",
